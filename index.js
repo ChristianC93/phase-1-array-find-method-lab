eeb172570f175333d455d14226b79972b3b3355c
Christian Castillo <castillochristian3@gmail.com>
2022-02-10 12:07:40 -0500

    commit tree 0dec2ab90cd6ee585084d4e8ef65214456438442

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array){
+   const win = array.find(array => array.result === "W")
+    if (win === undefined){
+        return win;
+    } else {
+        return win.year
+    }
+}
